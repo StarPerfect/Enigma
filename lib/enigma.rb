ec7bfc0d8af97537fbefcae966ab7980d02b1c74
Corina Allen <StarPerfect@gmail.com>
2019-07-26 11:36:46 -0600

    Update Enigma class with new modules

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,13 +1,15 @@
 class Enigma
-  attr_reader :alphabet, :key, :date
+  include RandomFive
+  include SixDigitDate
+  attr_reader :alphabet#, :key, :date
 
   def initialize
     @alphabet = ('a'..'z').to_a << " "
-    @key = key || Key.generate
-    @date = date || Offset.generate
+    # @key = key || key_generate
+    # @date = date || six_digit_date
   end
 
-  def encrypt(message, key, date)
-    
+  def encrypt(message, key = key_generate, date = six_digit_date)
+
   end
 end
