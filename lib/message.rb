388984cd9f485a262968b4e07488db0c17e73245
Corina Allen <StarPerfect@gmail.com>
2019-07-27 07:40:09 -0600

    Create Message class to handle actual message encryption with hashes for each A-D cipher

--- lib/message.rb
+++ lib/message.rb
@@ -6,4 +6,40 @@ class Message
     @shift = shift
     @alphabet = ('a'..'z').to_a << " "
   end
+
+  def a_cipher
+    a_cipher_hash = Hash.new
+    a_cipher_array = @alphabet.rotate(@shift[0])
+    @alphabet.each{ |char| a_cipher_hash[char] = a_cipher_array[@alphabet.index(char)]}
+    a_cipher_hash
+  end
+
+  def b_cipher
+    b_cipher_hash = Hash.new
+    b_cipher_array = @alphabet.rotate(@shift[0])
+    @alphabet.each{ |char| b_cipher_hash[char] = b_cipher_array[@alphabet.index(char)]}
+    b_cipher_hash
+  end
+
+  def c_cipher
+    c_cipher_hash = Hash.new
+    c_cipher_array = @alphabet.rotate(@shift[0])
+    @alphabet.each{ |char| c_cipher_hash[char] = c_cipher_array[@alphabet.index(char)]}
+    c_cipher_hash
+  end
+
+  def d_cipher
+    d_cipher_hash = Hash.new
+    d_cipher_array = @alphabet.rotate(@shift[0])
+    @alphabet.each{ |char| d_cipher_hash[char] = d_cipher_array[@alphabet.index(char)]}
+    d_cipher_hash
+  end
+
+  def encrypt
+
+  end
+
+  def decrypt
+
+  end
 end
--- test/message_test.rb
+++ test/message_test.rb
@@ -5,10 +5,16 @@ class MessageTest < Minitest::Test
     @key = Key.new('02715')
     @offset = Offset.new('040895')
     @shift = Shift.new(@key, @offset)
-    @plain_text = Message.new('Corina Rae Allen!', @shift.final_shift)
+    @plain_text = Message.new('hello world', @shift.final_shift)
   end
 
-  def test_character_set
+  def test_message_exists
+    assert_instance_of  Message, @plain_text
+  end
+
+  def test_attributes
+    assert_equal 'hello world', @plain_text.message
+    assert_equal  , @shift.final_shift
     assert_equal 27, @plain_text.alphabet.length
   end
 end
