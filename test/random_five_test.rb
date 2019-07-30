4e72769dd0fb98c810e58b820a9c7c02f75f6d89
Corina Allen <StarPerfect@gmail.com>
2019-07-29 20:55:55 -0600

    Add rjust test

--- test/random_five_test.rb
+++ test/random_five_test.rb
@@ -5,4 +5,11 @@ class RandomFiveTest < Minitest::Test
     enigma = Enigma.new
     assert_equal 5, enigma.key_generate.length
   end
+
+  def test_rjust
+    five_digits = 43
+    five_digits = five_digits.to_s
+
+    assert_equal '00043', five_digits.rjust(5, '0')
+  end
 end
