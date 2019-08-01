0b04889f8e7e80d4b63528d4a53a4861a7170d55
Corina Allen <StarPerfect@gmail.com>
2019-07-31 18:18:27 -0600

    Still need to finish stubbing out the tests to encrypt wehn no key or date provided

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -28,4 +28,23 @@ class EnigmaTest < Minitest::Test
 
     assert_equal expected, @enigma.decrypt('keder ohulw', '02715', '040895')
   end
+
+  def test_encrypt_without_key_or_offset
+    key = mock('Key')
+    key.stubs(:five_digits).returns('12345')
+    offset = mock('Offset')
+    offset.stubs(:six_digit_date).returns('012345')
+    offset.stubs(:date).returns('012345')
+    enigma = Enigma.new
+
+    expected = {
+      encryption: 'xk ehx',
+      key: '12345',
+      date: '012345'
+    }
+
+    # assert_equal expected, enigma.encrypt('corina', key.five_digits, offset.date)
+    assert_equal expected, enigma.encrypt('corina', key.five_digits)
+    assert_equal expected, enigma.encrypt('corina')
+  end
 end
